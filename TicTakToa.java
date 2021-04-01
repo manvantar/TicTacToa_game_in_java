7ba9f5625e517df6811563f229ee5fb65b6ba158
manvantar <manukvshetty@gmail.com>
2021-04-01 09:35:59 +0530

    logic to choose user between x or o to play

--- TicTakToa.java
+++ TicTakToa.java
@@ -1,17 +1,31 @@
 package com.bridgelabz.tictactoe_game;
-
+import java.util.*;
 public class TicTakToa {
 
 	char[] createArray(int size){
-
 		char []a=new char[size];
-
 		for(int i=1;i<size;i++) {
 			a[i]=' ';	
 		}
-
 		return a;
 	}
 
+	char chooseXorO(){
+		char xoro=' ';
 
+		while(xoro!='x' || xoro !='0' || xoro !='X'||xoro!='O') {
+			System.out.println("Player 1 select X or O");
+			Scanner s=new Scanner(System.in);
+			xoro=s.next().charAt(0);
+		}
+		if(xoro=='X') {
+			xoro='x';
+		}
+		else if(xoro=='O') {
+			xoro='o';
+		}
+		
+		return xoro;
+
+	}
 }
